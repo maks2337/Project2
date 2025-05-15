b06d495951a39ffcddce2d752c6e9aef1dcc6393
maks2337 <maksimzinovev13031@gmail.com>
2025-05-15 12:23:50 +0300

    Добавьте файлы проекта.

---
+++ Project2/FileName.cpp
@@ -0,0 +1,18 @@
+﻿//ПРАКТИЧЕСКАЯ РАБОТА № 2. Организация данных языка программирования С/С++ 1 задание программа определения объема цилиндра
+#define _USE_MATH_DEFINES
+#include<cmath>
+#include<iostream>
+using namespace std;
+int main() {
+	float r = 0, h = 0, v = 0;
+	setlocale(LC_ALL, "rus");
+	cout << "Введите исходные данные:" << endl;
+	cout << "Радиус основания (см) -> ";
+	cin >> r;
+	cout << "Высоту цилиндра (см) -> ";
+	cin >> h;
+	v = M_PI * h * r * r;
+	cout << "Объем цилиндра " << v << "куб. см";
+	system("pause");
+	return 0;
+}
